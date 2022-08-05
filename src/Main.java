e8d656bf702e955227b4794ae807de0eed578b1e
Bahdan Rutskou <>
2022-08-05 11:47:55 +0300

    New human was added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Human vasia = new Human();
         Human petia = new Human();
+        Human petka = new Human();
     }
 }
