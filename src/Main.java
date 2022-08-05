eb306fc45120ab5b22a674a1fddcb4f8d57e848b
Bogdan Rutskov <rutkovba@gmail.com>
2022-08-05 12:05:39 +0300

    New humans were added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Human vasia = new Human();
         Human petia = new Human();
+        Human petka = new Human();
+        Human vaska = new Human();
         Human oleg = new Human();
     }
 }
